d9c7ee62b994f2cd6eae447ec7d37db0f71de690
Nugraha <mangcoding@gmail.com>
2022-12-17 20:54:58 +0700

    contoh function

--- php-3/Function.php
+++ php-3/Function.php
@@ -8,6 +8,7 @@ function hallo() {
     return 'hallo dunia';
 }
 
+//Contoh fungsi dengan parameter default
 function hitungLuasPersegiPanjang($panjang, $lebar=15) {
     $luas = $panjang * $lebar;
     return $luas;
@@ -22,3 +23,20 @@ echo "<br>";
 echo hitungLuasPersegi(10);
 echo "<br>";
 echo hitungLuasPersegi(5);
+echo "<br>";
+
+
+
+function konversiSuhu($suhu, $dari="C", $ke="R") {
+    if ($dari == "C" && $ke=="R") {
+        $hasil = $suhu * 4/5;
+    } else if ($dari == "R" && $ke=="C") {
+        $hasil = $suhu * 5/4;
+    } else {
+        $hasil = "Tidak bisa dikonversi";
+    }
+
+    return $hasil;
+}
+
+echo "Hasil Konversi dari Suhu 100C ke Reamaur adalah ".konversiSuhu(100);
